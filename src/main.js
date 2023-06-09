e67c43b1561c26700861b43621de3fc666482e65
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-06-09 10:26:33 +0530

    asyn and aait .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 async function sum(n1, n2) {
+  // ...
   return n1 + n2;
 }
 
-let result = sum(1, 1);
-console.log(result);
+async function main() {
+  let result = await sum(1, 1);
+  console.log(result);
+}
+
+main();
